2c9d1f3ac072a946552010c69aee3be3d1326346
Godfrey Bongomin <bongomin@uw.edu>
2022-06-28 18:51:29 -0700

    Added a getter method in Thought model to format the timestamp

--- model/Thought.js
+++ model/Thought.js
@@ -1,11 +1,19 @@
 const { Schema, model } = require("mongoose");
 
-const thoughtSchema = new Schema({
-  thoughtText: { type: String, required: true, minlength: 1, maxlength: 280 },
-  createdAt: { type: Date, default: Date.now }, //Use a getter method to format the timestamp on query
-  username: { type: String, required: true },
-  reactions: [reactionSchema],
-});
+const thoughtSchema = new Schema(
+  {
+    thoughtText: { type: String, required: true, minlength: 1, maxlength: 280 },
+    createdAt: { type: Date, default: Date.now }, //Use a getter method to format the timestamp on query
+    username: { type: String, required: true },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
 
 // creating a virtual property 'reactionCount' that retrieves the length of the user's friends array field on query.
 thoughtSchema.virtual("reactionCount").get(() => {
